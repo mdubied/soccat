9274732fc68c04b80b8d5d331b6ed7495be75b06
Mathieu Dubied <mdubied@ethz.ch>
2026-05-22 17:49:37 +0200

    Correct top group manual annotation

--- tables/manual_annotation_top_groups.py
+++ tables/manual_annotation_top_groups.py
@@ -6,7 +6,7 @@ Read manual annotation file and compute the top-N most frequent "specific_group_
 separately for France and Germany, then export a side-by-side LaTeX table.
 
 Input:
-- data/manual_annotations/annotations_ground_truth.csv
+- data/manual_annotations/step_2/annotations_ground_truth.csv
 
 Output:
 - tables/manual_annotation_top_XX.tex   (XX = top_n argument)
